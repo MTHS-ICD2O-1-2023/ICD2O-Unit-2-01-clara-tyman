b00d9030286666750098c597ac0df9a641ad475f
Clara <144264170+clara-tyman@users.noreply.github.com>
2024-03-13 20:44:49 +0000

    html css js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Clara All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Clara tyman
+// Created on: mar 2024
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  document.getElementById("hello-world").innerHTML = "Hello, World!"
+}
